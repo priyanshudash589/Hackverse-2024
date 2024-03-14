8b8e8e234a44f4fac3d284ec01f7d533c31052ca
Suhas Srinivas Lingam <95020208+SuhasLingam@users.noreply.github.com>
2024-03-14 15:51:56 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import DimoHack from "../public/images/dimo-hack.svg";
+import heroRight from "../public/images/hero-r-logo.svg";
+import heroLeft from "../public/images/hero-l-logo.svg";
 import Link from "next/link";
 
 const Hero = () => {
+ 
+
   return (
     <div
       className="h-fix flex items-center flex-col justify-center"
@@ -14,15 +18,20 @@ const Hero = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="flex items-center flex-col gap-4">
+      <div className="flex items-center flex-col gap-4 md:-mt-[100px]">
+        
         <div className="section flex flex-col justify-center items-center h-[10rem] w-[25rem] mb-6">
-            <Image src={DimoHack} width={280} alt="DimoHack" />
+            <Image src={DimoHack} width={180} alt="DimoHack" />
             <p className="mt-4 mb-2">P r e s e n t s</p>
         </div>
-        <h1 className="tracking-wider text-center font-bold text-2xl md:text-6xl lg:text-7xl xl:text-7xl bg-gradient bg-clip-text text-transparent cursor-default">
-          H A C K V E R S E
+        <div className="flex justify-center items-center gap-[100px]">
+        <Image src={heroLeft} width={180} alt="hero-l" />
+        <h1 className="tracking-wider text-center font-extrabold text-2xl md:text-6xl lg:text-[170px] bg-gradient bg-clip-text text-transparent">
+           HACKVERSE 
         </h1>
-        <p className="tracking-widest text-center">{`The Next Big Thing`}</p>
+        <Image src={heroRight} width={180} alt="hero-r" />
+        </div>
+        <p className="tracking-widest text-center">{`TamilNadu's Largest Web3 Hackathon`}</p>
         <p className="tracking-widest text-center">April 06 & 07, 2024</p>
 
         <Link href="https://hackverse2024.devfolio.co/">
@@ -32,7 +41,10 @@ const Hero = () => {
         >
           Apply Now
         </Button>
+          <button>
+         </button>
         </Link>
+        
       </div>
     </div>
   );
