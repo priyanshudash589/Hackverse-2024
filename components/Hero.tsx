f65aa3b0f74992f90a6956fb70b0019af38d35b9
jaskrrish <jashehz@gmail.com>
2024-03-18 21:17:14 +0530

    Apply Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,8 +6,6 @@ import heroLeft from "../public/images/hero-l-logo.svg";
 import Link from "next/link";
 
 const Hero = () => {
- 
-
   return (
     <div
       className="h-fix flex items-center flex-col justify-center"
@@ -19,36 +17,31 @@ const Hero = () => {
       }}
     >
       <div className="flex items-center flex-col gap-4 md:-mt-[100px]">
-        
         <div className="section flex flex-col justify-center items-center h-[10rem] w-[25rem] mb-6">
-            <Image src={DimoHack} width={180} alt="DimoHack" />
-            <p className="mt-4 mb-2">P r e s e n t s</p>
+          <Image src={DimoHack} width={180} alt="DimoHack" />
+          <p className="mt-4 mb-2">P r e s e n t s</p>
         </div>
         <div className="flex justify-between items-center md:gap-12 lg:gap-15">
-
           <Image className="hidden md:flex " src={heroLeft} alt="hero-l" />
-      
+
           <h1 className="tracking-wider text-center font-extrabold text-3xl md:text-7xl lg:text-7xl xl:text-[8rem] bg-gradient bg-clip-text text-transparent">
-           HACKVERSE 
+            HACKVERSE
           </h1>
 
-          <Image className="hidden md:flex" src={heroRight}  alt="hero-r" />
+          <Image className="hidden md:flex" src={heroRight} alt="hero-r" />
+        </div>
+        <p className="tracking-widest text-center">{`TamilNadu's Largest Web3 Hackathon`}</p>
+        <p className="tracking-widest text-center">April 06 & 07, 2024</p>
 
-          </div>
-          <p className="tracking-widest text-center">{`TamilNadu's Largest Web3 Hackathon`}</p>
-          <p className="tracking-widest text-center">April 06 & 07, 2024</p>
-
-          <Link href="https://hackverse2024.devfolio.co/">
+        <Link href="https://hackverse2024.devfolio.co/">
           <Button
-          className="tracking-widest font-bold uppercase text-center"
-          variant={"banner"}
+            className="tracking-widest font-bold uppercase border-2 border-white bg-gradient-to-b from-[#230048] to-[#004945] text-center text-white hover:bg-gradient-to-r"
+            variant={"banner"}
           >
-          Apply Now
+            Apply Now
           </Button>
-            <button>
-            </button>
-          </Link>
-        
+          <button></button>
+        </Link>
       </div>
     </div>
   );
