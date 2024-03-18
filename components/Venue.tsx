ab679054badfd05aa1a3027f51be78dca8359211
jaskrrish <jashehz@gmail.com>
2024-03-18 22:50:55 +0530

    Add Venue Section

--- components/Venue.tsx
+++ components/Venue.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Venue = () => {
-    return (
-        <div
-            className="h-fix flex items-center flex-col justify-center"
-            style={{
-                backgroundImage: `url('./images/hero-bg.png')`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}
-        >
-            <div className="flex items-center justify-center h-screen">
-                <div className="w-1/2 bg-black border-2 border-D8B4FF rounded-md p-20 relative flex flex-col justify-center">
-                    <p className="text-white text-6xl font-bold bg-gradient-to-r from-19FB9B to-8C01FA text-center mb-4">VENUE</p>
-                    <p className="text-white text-xl text-center">Yet to be disclosed</p>
-                </div>
-            </div>
+  return (
+    <div
+      className="h-fix flex items-center flex-col justify-center"
+      style={{
+        backgroundImage: `url('./images/hero-bg.png')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-[50rem] bg-black border border-[#D8B4FF] rounded-md pt-[3rem] p-[4rem] relative flex flex-col justify-center">
+          <p className="text-5xl text-left font-bold bg-gradient bg-clip-text text-transparent mb-8">
+            VENUE
+          </p>
+          <p className="text-white m-2 text-2xl text-left">
+            Yet to be disclosed
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Venue;
