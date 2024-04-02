6a2de9ca03cb75defb4ffaf56f5d86088e3fbcd3
kalpithasv <kalpithasv@gmail.com>
2024-04-02 14:32:23 +0530

    kalps-partner

--- lib/constants.ts
+++ lib/constants.ts
@@ -29,6 +29,7 @@ export const sponsors = {
   diamond: ["diamond1.svg", "diamond2.svg","diamond3.svg"],
   gold: ["gold1.svg", "gold2.jpg", "gold3.svg", "dev-Gold.svg", "gold5.svg"],
   partners: ["/images/partner1.png", "/images/partner2.jpg", "/images/partner3.jpg", "/images/partner4.jpeg",
-    "/images/partner5.png", "/images/partner6.png",],
+    "/images/partner5.png", "/images/partner6.png", "/images/partner7.png", "/images/partner8.png",
+    "/images/partner9.png", "/images/partner10.jpeg", "/images/partner11.jpeg",],
 };
 
